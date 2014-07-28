1e367f545b472aebb1a301b1d4b17ac605fc16d5
Sander Bakkum <sander@sanderbakkum.nl>
2014-07-28 19:31:57 +0200

    Lots of stuff

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var net = require('net'),
-	artemisIP = 'localhost',
+	gui = require('./gui.js').gui;
+
+var artemisIP   = 'localhost',
 	artemisPort = 2011,
-	listenIP = '0.0.0.0',
-	listenPort = 2010;
+	listenIP    = '0.0.0.0',
+	listenPort  = 2010;
+
+var clients = [];
 
 var Enummer = { 'origin' : {
 					'server': 0x01,
@@ -33,6 +37,8 @@ var Enummer = { 'origin' : {
 
 var clientConnectionHandler = function (clientConnection) {
 
+	var instance = { };
+
 	console.log('New connection');
 
 	var clientData = new Buffer(0);
@@ -45,6 +51,8 @@ var clientConnectionHandler = function (clientConnection) {
 		clientConnection.pipe(serverConnection);
 		serverConnection.pipe(clientConnection);
 
+		clients.push(instance);
+
 		serverConnection.on('data',  serverDataReceived);
 		serverConnection.on('error', serverClosed);
 		serverConnection.on('close', serverClosed);
@@ -54,24 +62,27 @@ var clientConnectionHandler = function (clientConnection) {
 	var clientClosed = function () {
 		if (serverConnection)
 			serverConnection.end();
+
+		cleanUp();
 	}
 
 	var serverClosed = function () {
 		clientConnection.end();
+		cleanUp();
+	}
+
+	var cleanUp = function () {
+		clients.splice(clients.indexOf(instance), 1);
 	}
 
 	var clientDataReceived = function (data) {
 		clientData = Buffer.concat([clientData, data]);
 		clientData = parseArtemisStream(clientData, parsePacket);
-
-		console.log('SERVER <-- CLIENT: ' + hexify(data));
 	}
 
 	var serverDataReceived = function (data) {
 		serverData = Buffer.concat([serverData, data]);
 		serverData = parseArtemisStream(serverData, parsePacket);
-
-		console.log('SERVER --> CLIENT: ' + hexify(data));
 	}
 
 	var parseArtemisStream = function (data, cb) {
@@ -97,6 +108,8 @@ var clientConnectionHandler = function (clientConnection) {
 		var origin = data.readInt32LE(8);
 		var packetType = data.readInt32LE(20);
 
+		// console.log('SERVER' + (origin == Enummer.origin.client ? ' <-- ' : ' --> ') + 'CLIENT: ' + hexify(data));
+
 		if (origin == Enummer.origin.client) {
 			if (packetType == Enummer.packets.shipactionpacket.header) {
 				var subType = data.readInt32LE(24);
@@ -104,15 +117,17 @@ var clientConnectionHandler = function (clientConnection) {
 				if (subType == subs.setship) {
 					shipIndex = data.readInt32LE(28);
 				}
+
 				// add/remove consoles from client
 				if (subType == subs.setconsole){
-				    var console 	= data.readInt32LE(28);
+				    var consoleType = data.readInt32LE(28);
 					var selected 	= data.readInt32LE(32);
-					var consoleIndex = consoles.indexOf(console);
-					if(selected && consoleIndex === -1 ){
-						consoles.push(console);
-					}else if(!selected && consoleIndex !== -1){
-						consoles.splice(consoleIndex,1);
+					var consoleIndex = consoles.indexOf(consoleType);
+
+					if (selected && consoleIndex === -1) {
+						consoles.push(consoleType);
+					} else if (!selected && consoleIndex !== -1) {
+						consoles.splice(consoleIndex, 1);
 					}
 				}
 			}
@@ -132,13 +147,16 @@ var clientConnectionHandler = function (clientConnection) {
 	var serverConnection = net.connect(
 		{ host: artemisIP, port: artemisPort }, serverConnected);
 
+	instance.serverConnection = serverConnection;
+
 	clientConnection.on('data',  clientDataReceived);
 	clientConnection.on('error', clientClosed);
 	clientConnection.on('close', clientClosed);
 	clientConnection.on('end',   clientClosed);
 }
 
-var server = net.createServer(clientConnectionHandler);
+var server    = net.createServer(clientConnectionHandler);
+var guiServer = new gui(clients);
 
 server.listen(listenPort, listenIP);
 console.log('Server listening...');
